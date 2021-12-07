2855470e2296618d007e04b619c4057c61cfa8c9
Sergey Shishkin <sergejshishkin@gmail.com>
2021-12-07 21:02:52 +0300

    launch game mechanics

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 let myCanvas;
+let startButton;
 
 function setup() {
+    background('#34568B')
     myCanvas = createCanvas(600, 700)
     myCanvas.parent('#canvas')
+    frameRate(15)
+    noLoop()
+
+    startButton = select('#startbutton');
+    startButton.mousePressed(startGame)
+
+}
+
+function startGame() {
+    loop()
 }
 
 function draw() {
     background('#34568B')
+    circle(random(width), random(height), 10)
 }
