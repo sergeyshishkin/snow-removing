9fa83aa4f03d2ff593c3f936fd36295aa10ea3be
Sergey Shishkin <sergejshishkin@gmail.com>
2021-12-08 21:07:52 +0300

    snow drift animation mvp

--- script.js
+++ script.js
@@ -22,14 +22,60 @@ function setup() {
 
 
 function draw() {
+    // Variant 2 for snow animation
+/*     for (let s = 0; s < 1000; s++) { // set constant for snowflakes quantity
+        noStroke()
+        circle(random(width), random(height), 2);
+    }
+ */
+
     background('#34568B')
 
-    for (let s = 0; s < 100; s++) { // set constant for snowflakes quantity
+    for (let s = 0; s < 1000; s++) { // set constant for snowflakes quantity
         snow[s] = new Snowflake();
     }
-    
+
+
     for (i of snow) {
     i.show();
+    }
+    
+    // https://p5js.org/reference/#/p5/translate
+    // https://p5js.org/reference/#/p5/scale
+    // https://p5js.org/reference/#/p5/push
+    // https://p5js.org/reference/#/p5/pop
+
+    //arc(x, y, w, h, start, stop, [mode])
+    
+    fill(255)
+    arc(200, height, width, 150, -PI, 0, CHORD);    
+
+    fill(240)
+    arc(400, height, width, 150, -PI, 0, CHORD);    
+}
+
+/* Гипотеза: сугроб - это объект класса snowDrift, фигура это arc.
+функция создает сугробы по таймингу.
+покопаться в транслейт, скейл, пуш, поп
+*/ 
+
+
+class snowDrift {
+    constructor() {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+        this.start = s;
+        this.stop = st;
+        this.mode = mode;
+        this.color = color;
+    }
+
+    showArc() {
+        fill(color);
+        noStroke();
+        arc(x, y, w, h, s, st, CHORD)
     }
 
 }
@@ -38,7 +84,7 @@ class Snowflake {
     constructor() {
         this.x = random(width)
         this.y = random(height)
-        this.r = 5; /* Need replacement by relative unit*/
+        this.r = 2; // Need replacement by relative unit
     }
 
     show() {
@@ -46,4 +92,5 @@ class Snowflake {
         noStroke();
         circle(this.x, this.y, this.r)
     }
+ 
 }
