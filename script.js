80e0457205fee53809721a5edae454db4d509581
Sergey Shishkin <sergejshishkin@gmail.com>
2021-12-08 19:51:27 +0300

    add snow animation

--- script.js
+++ script.js
@@ -1,23 +1,49 @@
 let myCanvas;
 let startButton;
+let snow = [];
+
 
 function setup() {
     background('#34568B')
     myCanvas = createCanvas(600, 700)
     myCanvas.parent('#canvas')
-    frameRate(15)
-    noLoop()
-
-    startButton = select('#startbutton');
-    startButton.mousePressed(startGame)
+    frameRate(15)    
+    //noLoop()
+    
+    //startButton = select('#startbutton');
+    //startButton.mousePressed(startGame)
 
 }
 
-function startGame() {
-    loop()
+/* function startGame() {
+    loop()       
 }
+ */
+
 
 function draw() {
     background('#34568B')
-    circle(random(width), random(height), 10)
+
+    for (let s = 0; s < 100; s++) { // set constant for snowflakes quantity
+        snow[s] = new Snowflake();
+    }
+    
+    for (i of snow) {
+    i.show();
+    }
+
+}
+
+class Snowflake {
+    constructor() {
+        this.x = random(width)
+        this.y = random(height)
+        this.r = 5; /* Need replacement by relative unit*/
+    }
+
+    show() {
+        fill(255);
+        noStroke();
+        circle(this.x, this.y, this.r)
+    }
 }
